06ddccff5e50954f3e077b6362315b0e5928ebde
tfzx <2362595573@qq.com>
2025-03-31 20:02:01 +0800

    complete the workflow

--- search_boxes.py
+++ search_boxes.py
@@ -1,12 +1,12 @@
 import cupy as cp
 import numpy as np
 import MDAnalysis as mda
-from ARPDF import compute_ARPDF
-from utils import generate_grids, cosine_similarity
+from ARPDF import compute_ARPDF, show_ARPDF
+from utils import generate_grids, cosine_similarity, preprocess_ARPDF, to_cupy
 from utils import compute_axis_direction, adjust_ccl3_structure
 
 
-def search_structure(universe, ARPDF_exp, cutoff=10.0, N=512):
+def search_structure(universe, grids_XY, ARPDF_exp, cutoff=10.0, N=512):
 
     def sample_center_molecules():
         """ Return a list of atoms indices of molecules """
@@ -29,7 +29,7 @@ def search_structure(universe, ARPDF_exp, cutoff=10.0, N=512):
 
         molecule_atoms = u2.select_atoms(f"resid {mol_number}")
         target_c = molecule_atoms.select_atoms("name C")[0]  # 取第一个 C 原子
-        other_cls = molecule_atoms.select_atoms(f"name Cl and index != {molecule}")  # 其他 Cl 原子
+        other_cls = molecule_atoms.select_atoms(f"name Cl and not (index {molecule})")  # 其他 Cl 原子
 
         if len(other_cls) < 3:
             raise ValueError(f"分子 {mol_number} 中 Cl 原子数量不足，无法调整 CCl₃ 结构！")
@@ -43,8 +43,8 @@ def search_structure(universe, ARPDF_exp, cutoff=10.0, N=512):
 
         return [(polar_axis, u2, modified_atoms)]
 
-    X, Y = generate_grids(cutoff, N)
     molecule_list = sample_center_molecules()
+    grids_XY = to_cupy(grids_XY)
     results = {}
     for molecule in molecule_list:
         # TODO: parallelize this loop
@@ -53,7 +53,7 @@ def search_structure(universe, ARPDF_exp, cutoff=10.0, N=512):
         best_u2 = None
         best_ARPDF = None
         for polar_axis, u2, modified_atoms in generate_u2(molecule):
-            ARPDF = compute_ARPDF(universe, u2, cutoff, N, grids_XY=(X, Y), modified_atoms=modified_atoms, 
+            ARPDF = compute_ARPDF(universe, u2, cutoff, N, grids_XY, modified_atoms=modified_atoms, 
                                                 polar_axis=polar_axis, verbose=False)
             similarity = cosine_similarity(ARPDF, ARPDF_exp)
             if similarity > best_similarity:
@@ -61,21 +61,33 @@ def search_structure(universe, ARPDF_exp, cutoff=10.0, N=512):
                 best_similarity = similarity
                 best_u2 = u2
                 best_ARPDF = ARPDF
-        results[tuple(molecule)] = (best_polar_axis, best_u2, best_ARPDF, best_similarity)
+        results[molecule] = (best_polar_axis, best_u2, best_ARPDF, best_similarity)
     return results
 
 def workflow_demo():
     def get_box_iter():
         """Run and sample boxes from the MD simulation."""
-        pass
+        return [mda.Universe('data/CCl4/CCl4.gro')]
     def get_universe(box):
-        pass
-    def load_ARPDF_exp():
-        pass
+        return box
+    def load_ARPDF_exp(file_name):
+        ARPDF_exp_raw = np.load(file_name)
+        ori_range = 9.924650203173275
+        X, Y, ARPDF_exp = preprocess_ARPDF(ARPDF_exp_raw, ori_range, rmax=9.0)
+        return X, Y, ARPDF_exp
     def dump_results(results):
+        best_mol = max(results, key=lambda x: results[x][3])
+        # for molecule, (polar_axis, u2, ARPDF, similarity) in results.items():
+        polar_axis, u2, ARPDF, similarity = results[best_mol]
+        print(f"Molecule {best_mol}: Similarity = {similarity}")
+        print(f"  Polar axis: {polar_axis}")
+        show_ARPDF(ARPDF, plot_rmax=9.0, show_rmax=8.0, max_intensity=0.1)
         pass
-    ARPDF_exp = load_ARPDF_exp()
+    X, Y, ARPDF_exp = load_ARPDF_exp("data/CCl4/ARPDF_exp.npy")
     for box in get_box_iter():
         universe = get_universe(box)
-        results = search_structure(universe, ARPDF_exp)
+        results = search_structure(universe, (X, Y), ARPDF_exp)
         dump_results(results)
+
+if __name__ == "__main__":
+    workflow_demo()
